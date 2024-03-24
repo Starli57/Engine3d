0f5a72d3530459581d8fba28023884fb17c5f488
Aleksandr Termer <termeralexandr@gmail.com>
2024-03-24 12:04:35 +0200

    3d rotation

--- Engine3d/Source/Systems/TransformSystem.cpp
+++ Engine3d/Source/Systems/TransformSystem.cpp
@@ -18,7 +18,9 @@ void TransformSystem::Update(float deltaTime)
 
 		uboModel = glm::mat4(1.0f);
 		uboModel = glm::translate(uboModel, position);
-		uboModel = glm::rotate(uboModel, glm::radians(rotation.z), glm::vec3(0.0f, 1.0f, 0.0f));
+		uboModel = glm::rotate(uboModel, glm::radians(rotation.x), glm::vec3(0.0f, 1.0f, 0.0f));
+		uboModel = glm::rotate(uboModel, glm::radians(rotation.y), glm::vec3(1.0f, 0.0f, 0.0f));
+		uboModel = glm::rotate(uboModel, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
 		uboModel = glm::scale(uboModel, scale);
 
 		uboComponent.model = uboModel;
