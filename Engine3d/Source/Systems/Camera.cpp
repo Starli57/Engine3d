1c5773e7b0373d9bff3561d8c5328ce80b23ed4b
Aleksandr Termer <termeralexandr@gmail.com>
2024-03-24 12:10:37 +0200

    use y as up axis for camera

--- Engine3d/Source/Systems/Camera.cpp
+++ Engine3d/Source/Systems/Camera.cpp
@@ -26,7 +26,7 @@ void Camera::Update(float deltaTime = 0)
 		auto& position = entities.get<PositionComponent>(entity).position;
 		auto& uboComponent = entities.get<UboViewProjectionComponent>(entity);
 
-		uboComponent.view = glm::lookAt(position, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		uboComponent.view = glm::lookAt(position, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		uboComponent.proj = glm::perspective(glm::radians(pov), screenAspectRatio, zNear, zFar);
 		uboComponent.proj[1][1] *= -1;
 	}
