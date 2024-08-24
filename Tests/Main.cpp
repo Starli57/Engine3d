d859acac8bf60e9e2c514849e1dd7985f44967cc
Aleksandr Termer <termeralexandr@gmail.com>
2024-08-24 17:52:43 +0300

    keep test results window open

--- Tests/Main.cpp
+++ Tests/Main.cpp
@@ -1,7 +1,9 @@
 #include <catch_amalgamated.hpp>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
     int result = Catch::Session().run(argc, argv);
+    std::cin.get();
     return 0;
 }
