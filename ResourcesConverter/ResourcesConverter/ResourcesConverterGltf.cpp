c9c524dc80653553a502f59edd97921d49d1b11d
Aleksandr Termer <termeralexandr@gmail.com>
2025-07-26 13:29:12 +0300

    link tinygltf

---
+++ ResourcesConverter/ResourcesConverter/ResourcesConverterGltf.cpp
@@ -0,0 +1,27 @@
+﻿#include "EngineCore/Pch.h"
+#include "ResourcesConverterGltf.h"
+#include "EngineCore/Serialization/EntitySerializer.h"
+
+#include <tiny_gltf.h>
+
+void ResourcesConverterGltf::ImportMesh(const std::string& meshPathStr, const std::filesystem::path& meshPath,
+    const std::string& inFolder, const std::string& outFolder, const std::string& directoryName,
+    std::unordered_map<std::string, std::filesystem::path>& texturesPaths)
+{
+    tinygltf::Model model;
+    tinygltf::TinyGLTF gltf_ctx;
+    std::string err;
+    std::string warn;
+    
+    bool ret = gltf_ctx.LoadASCIIFromFile(&model, &err, &warn, meshPathStr.c_str());
+
+    if (!warn.empty()) spdlog::warn(warn);
+    if (!err.empty()) spdlog::error(err);
+
+    if (!ret)
+    {
+        spdlog::critical("Failed to parse glTF by path: {}", meshPathStr);
+        return;
+    }
+    
+}
---
+++ ResourcesConverter/ResourcesConverter/ResourcesConverterGltf.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "ConvertingMeshData.h"
+#include "ResourcesConverter.h"
+
+class ResourcesConverterGltf : public ResourcesConverter
+{
+public:
+    void ImportFolder(const std::string& inFolder, const std::string& outFolder, const std::string& directoryName);
+    void ImportMesh(const std::string& meshPathStr, const std::filesystem::path& meshPath, const std::string& inFolder, 
+        const std::string& outFolder, const std::string& directoryName, std::unordered_map<std::string, std::filesystem::path>& texturesPaths);
+};
